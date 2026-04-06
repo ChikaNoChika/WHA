40be0b8d7a1953ffd70bbc28b9b56a9c9fe4579f
ChikaNoChika <172503565+ChikaNoChika@users.noreply.github.com>
2026-04-06 15:26:24 -0400

    Make mux script

---
+++ mux.py
@@ -0,0 +1,54 @@
+from muxtools import Setup, GlobSearch, SubFile, mux, Premux, Chapters, ASSHeader
+
+
+def make_mux(x):
+    setup = Setup(f"{x:02}", config_file="config.ini")
+
+    webdl = GlobSearch(f"./{setup.episode}/*-ToonsHub.mkv")
+
+    premux = Premux(
+        webdl,
+        video=-1,
+        audio=-1,
+        subtitles=None,
+        keep_attachments=False,
+        mkvmerge_args='--no-global-tags --track-name 0:"1080p H.264 WEB-DL [CR]" --track-name 1:"Japanese 2.0 AAC [CR]" --track-name 2:"English 2.0 AAC [CR]" --language 0:und --language 1:jpn --language 2:eng --default-track-flag 0:1 --default-track-flag 1:1 --default-track-flag 2:1 --original-flag 1:1',
+    )
+
+    dialogue = SubFile(f"./{setup.episode}/WHA - {setup.episode} - Dialogue.ass")
+
+    ts = SubFile(f"./{setup.episode}/WHA - {setup.episode} - Signs.ass")
+
+    full = dialogue.merge(ts)
+
+    dubtitles = SubFile.from_srt(
+        f"./{setup.episode}/WHA - {setup.episode} - Dubtitles.srt"
+    )
+
+    japanese = SubFile(f"./{setup.episode}/WHA - {setup.episode} - Japanese.ass")
+
+    chapters = Chapters.from_sub(full)
+
+    fonts = full.collect_fonts(use_system_fonts=True)
+
+    jp_fonts = japanese.collect_fonts(use_system_fonts=False)
+
+    mux(
+        premux,
+        full.to_track(name="Full Subtitles [Chika]", lang="eng"),
+        dubtitles.to_track(name="Dubtitles [NF]", lang="eng", default=False),
+        ts.to_track(
+            name="Signs & Songs [Chika]", lang="en", default=False, forced=True
+        ),
+        japanese.to_track(
+            name="Japanese Subtitles [SonicMaster]", lang="jpn", default=False
+        ),
+        chapters,
+        *fonts,
+        *jp_fonts,
+    )
+
+
+ep = int(input("Enter episode number: "))
+
+make_mux(ep)
